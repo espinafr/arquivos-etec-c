0e77deb4a03b72ef443b5db5b7607d2083469cc3
Glitch (gilded-filament) <none>
2023-08-21 16:19:46 +0000

    🌟🌖 Checkpoint ./public/client.js:8394924/901 ./server.py:8394924/1001 ./views/index.html:8394924/70 ./public/style.css:8394924/153

--- server.py
+++ server.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#       _                              
-#      | |                             
-#    __| |_ __ ___  __ _ _ __ ___  ___ 
-#   / _` | '__/ _ \/ _` | '_ ` _ \/ __|
-#  | (_| | | |  __/ (_| | | | | | \__ \
-#   \__,_|_|  \___|\__,_|_| |_| |_|___/ .
-#
-# A 'Fog Creek'–inspired demo by Kenneth Reitz™
-
 import os
 from flask import Flask, request, render_template, jsonify
 import test
@@ -24,7 +15,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', )
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
